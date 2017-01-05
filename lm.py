2d7789eeb8a5b26288cf692c62be7dc019212d24
Jon Dehdari <jon@dehdari.org>
2017-01-05 12:29:43 +0100

    lm.py++

--- lm.py
+++ lm.py
@@ -5,24 +5,35 @@
 """
 
 import sys
-import numpy as np
-import tensorflow as tf
+import match.data.io as io
 
 
 def main():
+    """ ... """
     import argparse
     parser = argparse.ArgumentParser(description='...')
 
     parser.add_argument('--batch_size', type=int, default=16, help="Batch size")
     parser.add_argument('-d', '--dev', type=str, help="Load dev/tuning file")
     parser.add_argument('--epochs', type=int, default=20, help="Maximum number of epochs")
-    parser.add_argument('-i', '--in', type=str, default=sys.stdin, help="Load input training file")
+    parser.add_argument('-i', '--in', dest='input', type=str, default=sys.stdin,
+            help="Load input training file")
     parser.add_argument('--learning_rate', type=float, default=0.001, help="Learning rate")
     parser.add_argument('-m', '--model', help="Filename of model to load and/or save")
-    parser.add_argument('-o', '--out', type=str, default=sys.stdout, help="")
+    parser.add_argument('-o', '--out', dest='output', type=str, default=sys.stdout, help="")
     parser.add_argument('--opt', type=str, default='adam', help="Optimizer")
     args = parser.parse_args()
     ...
+
+    # Load input data
+    data = io.Data()
+    print(data)
+    data.load(train_tgt=args.input)
+
+    # Build model
+
+    # Train model
+
 
 if __name__ == '__main__':
     main()
